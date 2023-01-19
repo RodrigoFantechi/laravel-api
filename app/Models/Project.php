e4950d40ce51b07a1d90bac080dc40e48cfe8d68
RodrigoFantechi <rodrifante595@libero.it>
2023-01-19 15:08:54 +0100

    initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+use App\Models\Type;
+use App\Models\Technology;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Project extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'description', 'slug', 'cover_image', 'type_id'];
+
+    public static function createSlug($input)
+    {
+        return Str::slug($input);
+    }
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+
+    /**
+     * The roles that belong to the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function technologies(): BelongsToMany
+    {
+        return $this->belongsToMany(Technology::class);
+    }
+}
